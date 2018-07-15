1eb712a52ce95b7ccc16f077b52e4bec565c5612
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2018-07-15 00:41:21 -0500

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { SMS } from '@ionic-native/sms';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+//debugger;
 @NgModule({
   declarations: [
     MyApp,
@@ -23,7 +25,7 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
-    SplashScreen,
+    SplashScreen, SMS,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SMS } from '@ionic-native/sms';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  name: string = "Krishna";
+  clickMessage: string = "this";
+  public smsResponse: string;
+ 
+  constructor(public navCtrl: NavController, private smsVar: SMS) {
 
   }
-
+  sendSms() {        
+    var options={
+          replaceLineBreaks: false, // true to replace \n by a new line, false by default
+          android: {
+              intent: 'INTENT'  // Opens Default sms app
+              //intent: '' // Sends sms without opening default sms app
+            }
+    }
+    this.smsVar.send('416123456', 'Hello world!',options)
+      .then(()=>{
+        alert("success");
+      },()=>{
+      alert("failed");
+      });
+  }
 }
