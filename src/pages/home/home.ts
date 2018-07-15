34dc6bcc3197a21409991d62fb5ed903aa2f35bd
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2018-07-15 17:45:00 -0500

    added btns

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SMS } from '@ionic-native/sms';
-
+import { ToastController } from 'ionic-angular';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,11 +10,20 @@ export class HomePage {
   name: string = "Krishna";
   clickMessage: string = "this";
   public smsResponse: string;
- 
-  constructor(public navCtrl: NavController, private smsVar: SMS) {
+  users: any[] = [
+    {
+      user: "krish",
+      ph: "6147875430"
+    },
+    {
+      user: "krish2",
+      ph: "5102798255"
+    }
+  ];
+  constructor(public navCtrl: NavController, private smsVar: SMS,public toastCtrl: ToastController) {
 
   }
-  sendSms() {        
+  sendSms(position, type) {        
     var options={
           replaceLineBreaks: false, // true to replace \n by a new line, false by default
           android: {
@@ -22,11 +31,27 @@ export class HomePage {
               //intent: '' // Sends sms without opening default sms app
             }
     }
-    this.smsVar.send('6147875430', 'Hello Krish',options)
-      .then(()=>{
-        alert("success");
-      },()=>{
-      alert("failed");
-      });
+    //debugger;
+    var messages = {
+      "alert": 'Alert! login and look into the bugs',
+      "warning": 'Warning! It doesn\'t seems like an emergency, but better to look into the issue',
+      "emergency": 'Emergency! hurry up',
+      "success": 'Success! the deployment went good'
+    };
+
+    const toast = this.toastCtrl.create({
+      message: 'User was added successfully',
+      duration: 3000
+    });
+    for(var i = 0, ulength = this.users.length; i < ulength ; i++) {
+      this.smsVar.send(this.users[i]["ph"], messages[type], options)
+        .then(()=>{
+          console.log("success");
+        },()=>{
+        console.log("failed");
+        });  
+    }
+
+    toast.present();
   }
 }
