210706110d9aa7d9a8451d759681e763c9f0f0be
Krishnakumar Rajendran <krishnasinbox@outlook.com>
2018-07-18 00:16:13 -0500

    updated ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,57 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SMS } from '@ionic-native/sms';
+import firebase from 'firebase';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { PopoverPage } from '../pages/popover/popover';
 
-//debugger;
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseconfig = {
+  apiKey: "AIzaSyDJNzRNPHAX21u-rjy7pqS3Tv6arFf3EME",
+  authDomain: "sendsms-55b02.firebaseapp.com",
+  databaseURL: "https://sendsms-55b02.firebaseio.com",
+  projectId: "sendsms-55b02",
+  storageBucket: "sendsms-55b02.appspot.com",
+  messagingSenderId: "1070525234045"
+};
+
+//firebase.initializeApp(firebaseconfig);
+
+debugger;
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    PopoverPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseconfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule  
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    PopoverPage
   ],
   providers: [
     StatusBar,
     SplashScreen, SMS,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
+
+// export const DB = firebase.database()
+// export const auth = firebase.auth()
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,29 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, NgZone } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SMS } from '@ionic-native/sms';
 import { ToastController } from 'ionic-angular';
+import { PopoverController } from 'ionic-angular';
+import { PopoverPage } from '../popover/popover';
+
+import { List } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+import { Storage } from '@ionic/storage';
+//import { DB } from '../../app/app.module';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild(List) list: List;
   name: string = "Krishna";
   clickMessage: string = "this";
   public smsResponse: string;
-  users: any[] = [
-    {
-      user: "krish",
-      ph: "6147875430"
-    },
-    {
-      user: "krish2",
-      ph: "5102798255"
-    }
-  ];
-  constructor(public navCtrl: NavController, private smsVar: SMS,public toastCtrl: ToastController) {
+  users: Observable<any[]>;
 
+  constructor(public navCtrl: NavController, private smsVar: SMS,
+    public toastCtrl: ToastController,
+    afDB: AngularFireDatabase,
+    public popoverCtrl: PopoverController,
+    private storage: Storage) {
+    var self = this;
+    this.isLoaded = false;
+    this.users = afDB.list('users').valueChanges();
+      console.log(this.users);
+    // ref.on("value", function(snapshot) {
+    //    self.refresh(snapshot.val());
+    //    storage.set('users', snapshot.val());
+    //    self.isLoaded = true;
+    // }, function(errorObj) {
+    //     self.isLoaded = false;
+    //     storage.get('users').then((val) => {
+    //       self.refresh(val);
+    //     });
+    // })  
   }
-  sendSms(position, type) {        
+  sendSms(position, user) {        
     var options={
           replaceLineBreaks: false, // true to replace \n by a new line, false by default
           android: {
@@ -31,27 +49,26 @@ export class HomePage {
               //intent: '' // Sends sms without opening default sms app
             }
     }
-    //debugger;
-    var messages = {
-      "alert": 'Alert! login and look into the bugs',
-      "warning": 'Warning! It doesn\'t seems like an emergency, but better to look into the issue',
-      "emergency": 'Emergency! hurry up',
-      "success": 'Success! the deployment went good'
-    };
-
+    //debugger;   
     const toast = this.toastCtrl.create({
       message: 'Message was sent successfully',
       duration: 3000
     });
-    for(var i = 0, ulength = this.users.length; i < ulength ; i++) {
-      this.smsVar.send(this.users[i]["ph"], messages[type], options)
-        .then(()=>{
-          console.log("success");
-        },()=>{
-        console.log("failed");
-        });  
-    }
-
+    
+    this.smsVar.send(user.ph, user.message)
+      .then(()=>{
+        console.log("success");
+      },()=>{
+      console.log("failed");
+      });  
+    
     toast.present();
+  }
+  presentPopover(myEvent) {
+    debugger
+    let popover = this.popoverCtrl.create(PopoverPage);
+    popover.present({
+      ev: myEvent
+    });
   }
 }
